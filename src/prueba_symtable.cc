d428f322616c861115c08137dc6a5b5cd8e5473b
gabrielformica <gabrielformica93@gmail.com>
2014-03-28 23:19:22 -0430

    se prueban mas cosas de la symtable

--- src/prueba_symtable.cc
+++ src/prueba_symtable.cc
@@ -7,16 +7,45 @@ using namespace std;
 
 
 int main(int argc, char **argv) {
-  Symbol *s = new Symbol("ola",1,2,3); 
+  int line = 0;
+  int column = 0;
+  Symbol *s1 = new Symbol("ola1",1,line,column); 
+  Symbol *s11 = new Symbol("ola1",2,15,column); 
+  Symbol *s2 = new Symbol("ola2",1,16,column); 
   SymbolTable *st = new SymbolTable();
+
+  /*se prueba el scope*/
   st->enterScope();
   int actualScope = st->getActualScope();
+  cout << "Aqui se va a probar el getActualScope. Debe ser igual a 1" << endl;
+  cout << "Actual Scope  ------>  " << actualScope << endl;
+  cout << endl; 
+  /*se termina prueba del scope*/
 
-  cout << "actual scope : " << actualScope << endl;
+  st->addSymbol(s1);
+  st->addSymbol(s2);
 
-  st->addSymbol(s);
-  Symbol *otro = st->lookup("ola",1);
+  /*se prueba lookup*/
+  Symbol *otro = st->lookup("ola1",1);
+  cout << "Aqui se va a probar el lookup. " << endl;
+  cout << endl; 
+  cout << "     El id siguiente debe ser 'ola1'" << endl;
+  cout << "     id   ------>  " << otro->getId() << endl;
+  cout << endl; 
+  cout << "     La linea debe ser igual 0" << endl;
+  cout << "     linea -------> " << otro->getLine() << endl;
+  cout << endl; 
+  cout << "     El scope debe ser 1" << endl;
+  cout << "     scope   ------>  " << otro->getScope() << endl;
 
-  cout << "Este es el id de otro : " << otro->getId() << endl;
+  st->addSymbol(s11);
+  Symbol *otro2 = st->lookup("ola1",2);
+  cout << "     El scope debe ser 2" << endl;
+  cout << "     scope   ------>  " << otro2->getScope() << endl;
+  cout << endl; 
+  cout << "     La linea debe ser igual 15" << endl;
+  cout << "     linea -------> " << otro2->getLine() << endl;
+  cout << endl; 
+  /*se termina prueba de lookup*/
 
 }
