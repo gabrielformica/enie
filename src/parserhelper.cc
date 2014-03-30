a57394aa9f96605a94febb26e7ae6033620e4f9d
gabrielformica <gabrielformica93@gmail.com>
2014-03-30 14:38:45 -0430

    se cambia string de errores en parserhelper

--- src/parserhelper.cc
+++ src/parserhelper.cc
@@ -15,8 +15,10 @@ void tryAddSymbol(SymbolTable *symtable, vector<string> *errors, Symbol *s) {
         cout << "El simbolo " << id << " esta en la linea : " << line << endl;
     }
     else {
-        std::string str = "error: variable '"+ id + "' ubicado en linea: ";
-        str+= to_string(line) + ", ya fue declarada antes";
+        std::string str0 = "(linea "+ to_string(line)+ ", columna ";
+        str0 += to_string(column) + "): ";
+        std::string str = "error "+ str0 + "variable '"+ id +"'";
+        str += ", ya fue declarada en este alcance";
         errors->push_back(str);
     }
 }
@@ -25,8 +27,11 @@ void checkUse(SymbolTable *symtable, vector<string> *errors, Symbol *s) {
     if (! symtable->isActive(s)) {
         std::string id = s->getId();
         int line = s->getLine();
-        std::string str = "error: variable '"+ id + "' ubicado en linea: ";
-        str += to_string(line) + ", no ha sido declarada";
+        int column = s->getColumn();
+        std::string str0 = "(linea "+ to_string(line)+ ", columna ";
+        str0 += to_string(column) + "): ";
+        std::string str = "error "+ str0 + "variable '"+ id +"'";
+        str += ", no ha sido declarada";
         errors->push_back(str);
     }
 }
