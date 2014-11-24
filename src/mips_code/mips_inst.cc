d93311cc9a0c5270593f9afe3b76a4dc88912aa6
Melecio Ponte <pontezambrano@gmail.com>
2014-11-24 08:23:15 -0430

    *Se implementan las clases de algunas instrucciones en mips.

---
+++ src/mips_code/mips_inst.cc
@@ -0,0 +1,191 @@
+/**
+  * Copyright 2014
+  *
+  * @file
+  *
+  * @author: Gabriel Formica <gabrielformica93@gmail.com>
+  * @author: Melecio Ponte <pontezambrano@gmail.com>
+  *
+  * @section Description
+  *
+  * MIPS Instruction class implementation
+  */
+
+Add::Add(std::string z, std::string x, std::string y) {
+    this->mode = direct;
+    this->res = z;
+    this->arg1 = x;
+    this->arg2 = y;
+}
+
+std::string Add::toString() {
+    std::string str = "";
+    str += "add " + res +  ", " + arg1 + ", " + arg2;
+    return str;
+}
+
+AddI::AddI(std::string z, std::string x, int i) {
+    this->res  = z;
+    this->arg1 = x;
+    this->arg2 = i; // this is an imm
+}
+
+std::string AddI::toString() {
+    std::string str = "";
+    str += "addi " + this->res +  ", " + this->arg1 + ", ";
+    str += std::to_string(this->arg2);
+    return str;
+}
+
+
+Sub(std::string z, std::string x, std::string y) {
+    this->arg1 = x;
+    this->arg2 = y;
+    this->res  = z;
+}
+
+std::string Sub::toString() {
+    std::string str = "";
+    str += "sub " + this->res +  ", " + this->arg1 + ", " + this->arg2;
+    return str;
+}
+
+
+Mult::Mult(std::string x, std::string y) {
+    this->arg1 = x;
+    this->arg2 = y;
+}
+
+std::string Mult::toString() {
+    std::string str = "";
+    str += "mult " + this->arg1 + ", " + this->arg2;
+    return str;
+}
+
+
+Div::Div(std::string x, std::string y) {
+    this->arg1 = x;
+    this->arg2 = y;
+}
+
+std::string Div::toString() {
+    std::string str = "";
+    str += "div " + this->arg1 + ", " + this->arg2;
+    return str;
+}
+
+
+Mfhi::Mfhi(std::string x) {
+    this->arg1 = x;
+}
+
+std::string Mfhi::toString() {
+    std::string str = "";
+    str += "mfhi " + this->arg1;
+    return str;
+}
+
+
+Mfhi::Mfhi(std::string x) {
+    this->arg1 = x;
+}
+
+std::string Mflo::toString() {
+    std::string str = "";
+    str += "mflo " + this->arg1;
+    return str;
+}
+
+Move::Move(std::string a1, std::string r) {
+    this->arg1 = a1;
+    this->res  = r;
+}
+
+std::string Move::toString() {
+    std::string str = "";
+    str += "move " + this->arg1 + ", " + this->arg2;
+    return str;
+}
+
+Jump::Jump(std::string l) {
+    this->label = l;
+}
+
+std::string Jump::toString() {
+    std::string str = "";
+    str += "j " + this->label;
+    return str;
+}
+
+Beq::Beq(std::string a1, std::string a2, std::string l) {
+    this->arg1 = a1;
+    this->arg2 = a2;
+    this->label = l;
+}
+
+std::string Beq::toString() {
+    std::string str = "";
+    str += "beq " + this->arg1 + ", " + this->arg2 + ", " + this->label;
+    return str;
+}
+
+Bne::Bne(std::string a1, std::string a2, std::string l) {
+    this->arg1 = a1;
+    this->arg2 = a2;
+    this->label = l;
+}
+
+std::string Bne::toString() {
+    std::string str = "";
+    str += "bne " + this->arg1 + ", " + this->arg2 + ", " + this->label;
+    return str;
+}
+
+Bge::Bge(std::string a1, std::string a2, std::string l) {
+    this->arg1 = a1;
+    this->arg2 = a2;
+    this->label = l;
+}
+
+std::string Bge::toString() {
+    std::string str = "";
+    str += "bge " + this->arg1 + ", " + this->arg2 + ", " + this->label;
+    return str;
+}
+
+Bgt::Bgt(std::string a1, std::string a2, std::string l) {
+    this->arg1 = a1;
+    this->arg2 = a2;
+    this->label = l;
+}
+
+std::string Bgt::toString() {
+    std::string str = "";
+    str += "bgt " + this->arg1 + ", " + this->arg2 + ", " + this->label;
+    return str;
+}
+
+Ble::Ble(std::string a1, std::string a2, std::string l) {
+    this->arg1 = a1;
+    this->arg2 = a2;
+    this->label = l;
+}
+
+std::string Ble::toString() {
+    std::string str = "";
+    str += "ble " + this->arg1 + ", " + this->arg2 + ", " + this->label;
+    return str;
+}
+
+Blt::Blt(std::string a1, std::string a2, std::string l) {
+    this->arg1 = a1;
+    this->arg2 = a2;
+    this->label = l;
+}
+
+std::string Blt::toString() {
+    std::string str = "";
+    str += "blt " + this->arg1 + ", " + this->arg2 + ", " + this->label;
+    return str;
+}
+
---
+++ src/mips_code/mips_inst.hh
@@ -0,0 +1,197 @@
+/**
+  * Copyright 2014
+  *
+  * @file
+  *
+  * @author: Gabriel Formica <gabrielformica93@gmail.com>
+  * @author: Melecio Ponte <pontezambrano@gmail.com>
+  *
+  * @section Description
+  *
+  * MIPS Instruction class definition
+  */
+
+#ifndef MIPS_INST_HH
+#define MIPS_INST_HH
+
+class MipsInst {
+
+};
+
+///////////////////////////////////Arithmetic///////////////////////////////////
+
+class Add: public MipsInst {
+    private:
+        std::string res;
+        std::string arg1;
+        std::string arg2;
+
+    public:
+        Add(std::string z, std::string x, std::string y);
+
+        void setArg1(std::string);
+        void setArg2(std::string);
+        void setRes (std::string);
+        std::string toString();
+};
+
+class AddI: public MipsInst {
+    private:
+        std::string res;
+        std::string arg1;
+        int arg2; // this is an imm
+
+    public:
+        AddI(std::string z, std::string x, int i);
+
+        void setArg1(std::string);
+        void setArg2(int);
+        void setRes(std::string);
+
+        std::string toString();
+};
+
+class Sub: public MipsInst {
+    private:
+        std::string arg1;
+        std::string arg2;
+        std::string res;
+
+    public:
+        Sub(std::string z, std::string x, std::string y);
+
+        void setArg1(std::string);
+        void setArg2(std::string);
+        void setRes(std::string);
+        std::string toString();
+};
+
+class Mult: public MipsInst {
+    private:
+        std::string arg1;
+        std::string arg2;
+    public:
+        Mult(std::string x, std::string y);
+        std::string toString();
+};
+
+
+class Div: public MipsInst {
+    private:
+        std::string arg1;
+        std::string arg2;
+    public:
+        Div(std::string x, std::string y);
+        std::string toString();
+};
+
+/////////////////////////////////////Moves/////////////////////////////////////
+
+class Mfhi: public MipsInst {
+    private:
+        std::string arg1;
+    public:
+        Mfhi(std::string x);
+        std::string toString();
+};
+
+class Mflo: public MipsInst {
+    private:
+        std::string arg1;
+    public:
+        Mfhi(std::string x);
+        std::string toString();
+};
+
+class Move: public MipsInst {
+    private:
+        std::string arg1;
+        std::string res;
+    public:
+        Move(std::string a1, std::string r);
+        std::string toString();
+};
+
+/////////////////////////////////////Jumps/////////////////////////////////////
+
+// This works for jump and jump and link?
+// Don't know #checklater
+class Jump: public MipsInst {
+    private:
+        std::string label;
+    public:
+        Jump(std::string l);
+        std::string toString();
+};
+
+class Beq: public MipsInst {
+    private:
+        std::string arg1;
+        std::string arg2;
+        std::string label;
+    public:
+        Beq(std::string a1, std::string a2, std::string l);
+        std::string toString();
+};
+
+class Bne: public MipsInst {
+    private:
+        std::string arg1;
+        std::string arg2;
+        std::string label;
+    public:
+        Bne(std::string a1, std::string a2, std::string l);
+        std::string toString();
+};
+
+class Bge: public MipsInst {
+    private:
+        std::string arg1;
+        std::string arg2;
+        std::string label;
+    public:
+        Bge(std::string a1, std::string a2, std::string l);
+        std::string toString();
+};
+
+class Bgt: public MipsInst {
+    private:
+        std::string arg1;
+        std::string arg2;
+        std::string label;
+    public:
+        Bgt(std::string a1, std::string a2, std::string l);
+        std::string toString();
+};
+
+class Ble: public MipsInst {
+    private:
+        std::string arg1;
+        std::string arg2;
+        std::string label;
+    public:
+        Ble(std::string a1, std::string a2, std::string l);
+        std::string toString();
+};
+
+
+class Blt: public MipsInst {
+    private:
+        std::string arg1;
+        std::string arg2;
+        std::string label;
+    public:
+        Blt(std::string a1, std::string a2, std::string l);
+        std::string toString();
+};
+
+
+/////////////////////////////////////Other/////////////////////////////////////
+
+class Syscall: public MipsInst {
+    public:
+        Syscall();
+        std::string toString();
+};
+
+#endif
